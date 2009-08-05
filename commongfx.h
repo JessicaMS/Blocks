292cfe4ef98c52abd3ea993ed4518acacfe853cd
JessicaMS <jessicams168@gmail.com>
2009-08-05 13:00:36 -0400

    add file

---
+++ commongfx.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#include <SDL/SDL_image.h>
+
+
+//❤`•.¸¸.•´´¯`••.¸¸.•´´¯`•´❤`•.¸¸.•´´¯`••.¸¸.•´´¯`•´❤`•.¸¸.•´´¯`••.¸¸.•´´¯`•´❤
+//function:  get_pixel32
+//
+//Receives a surface and a coordinate, then returns an integer containing
+//	the color of the pixel at that coordinate. 
+//❤`•.¸¸.•´´¯`••.¸¸.•´´¯`•´❤`•.¸¸.•´´¯`••.¸¸.•´´¯`•´❤`•.¸¸.•´´¯`••.¸¸.•´´¯`•´❤
+inline Uint32 get_pixel32( SDL_Surface *surface, int x, int y )
+{
+    //Convert the pixels to 32 bit
+    Uint32 *pixels = (Uint32 *)surface->pixels;
+    
+    //Get the requested pixel
+    return pixels[ ( y * surface->w ) + x ];
+}
+
+//❤`•.¸¸.•´´¯`••.¸¸.•´´¯`•´❤`•.¸¸.•´´¯`••.¸¸.•´´¯`•´❤`•.¸¸.•´´¯`••.¸¸.•´´¯`•´❤
+//function:  put_pixel32
+//
+//Receives a surface, coordinate, and color.  The function plots this color
+//	to the surface, and returns it byref. 
+//❤`•.¸¸.•´´¯`••.¸¸.•´´¯`•´❤`•.¸¸.•´´¯`••.¸¸.•´´¯`•´❤`•.¸¸.•´´¯`••.¸¸.•´´¯`•´❤
+inline void put_pixel32( SDL_Surface *surface, int x, int y, Uint32 pixel, int width, int height )
+{
+	//Convert the pixels to 32 bit
+	Uint32 *pixels = (Uint32 *)surface->pixels;
+
+	if (y>=0 && y<height && x>=0 && x<width)
+	{
+		//Set the pixel
+		pixels[ ( y * surface->w ) + x ] = pixel;
+	}
+}
+inline SDL_Surface *load_image( std::string filename ) 
+{
+	//The image that's loaded
+	SDL_Surface* loadedImage = NULL;
+
+	//The optimized surface that will be used
+	SDL_Surface* optimizedImage = NULL;
+
+	//Load the image
+	loadedImage = IMG_Load( filename.c_str() );
+
+	//If the image loaded
+	if( loadedImage != NULL )
+	{
+		//Create an optimized surface
+		optimizedImage = SDL_DisplayFormat( loadedImage );
+
+		//Free the old surface
+		SDL_FreeSurface( loadedImage );
+
+		//If the surface was optimized
+		if( optimizedImage != NULL )
+		{
+		        //Color key surface
+		        SDL_SetColorKey( optimizedImage, SDL_RLEACCEL | SDL_SRCCOLORKEY, SDL_MapRGB( optimizedImage->format, 0, 0xFF, 0x00) );
+		}
+	}
+
+	//Return the optimized surface
+	return optimizedImage;
+}
+
+
+inline void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination, SDL_Rect* clip = NULL )
+{
+    //Holds offsets
+    SDL_Rect offset;
+    
+    //Get offsets
+    offset.x = x;
+    offset.y = y;
+    
+    //Blit
+    SDL_BlitSurface( source, clip, destination, &offset );
+}
+
